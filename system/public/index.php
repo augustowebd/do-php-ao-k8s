c9085987937abb1f32a53a81b638c1633ccfeeba
J. Augusto <augustowebd@gmail.com>
2020-07-13 21:21:56 -0300

    refatoração de pastas

---
+++ system/public/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Página inicial da aplicação</title>
+</head>
+<body>
+
+    <a href="addUser.php">Registrar novo usuário</a>
+
+</body>
+</html>
